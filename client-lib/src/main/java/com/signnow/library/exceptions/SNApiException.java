b91bb9698ceae256435c9a5bda9ad4bebe9d49e7
Stefanos Chalkidis <s.chalkidis@protonmail.com>
2022-05-26 16:27:12 +0300

    formatted to google java style

--- client-lib/src/main/java/com/signnow/library/exceptions/SNApiException.java
+++ client-lib/src/main/java/com/signnow/library/exceptions/SNApiException.java
@@ -1,36 +1,37 @@
 package com.signnow.library.exceptions;
 
 import com.signnow.library.dto.ApiError;
-
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class SNApiException extends SNException {
-    final transient List<ApiError.ErrorInfo> errorInfo;
+  final transient List<ApiError.ErrorInfo> errorInfo;
 
-    public SNApiException(List<ApiError.ErrorInfo> errors) {
-        this.errorInfo = errors;
-    }
+  public SNApiException(List<ApiError.ErrorInfo> errors) {
+    this.errorInfo = errors;
+  }
 
-    @Override
-    public String getMessage() {
-        String mess = super.getMessage();
-        if (mess != null) {
-            return mess;
-        }
-        if (errorInfo != null) {
-            return errorInfo.stream().map(e -> e.code + ": " + e.message).collect(Collectors.joining("\\n"));
-        }
-        return null;
+  @Override
+  public String getMessage() {
+    String mess = super.getMessage();
+    if (mess != null) {
+      return mess;
     }
+    if (errorInfo != null) {
+      return errorInfo.stream()
+          .map(e -> e.code + ": " + e.message)
+          .collect(Collectors.joining("\\n"));
+    }
+    return null;
+  }
 
-    public SNApiException(String message) {
-        super(message);
-        this.errorInfo = null;
-    }
+  public SNApiException(String message) {
+    super(message);
+    this.errorInfo = null;
+  }
 
-    public SNApiException(String message, Throwable cause) {
-        super(message, cause);
-        this.errorInfo = null;
-    }
+  public SNApiException(String message, Throwable cause) {
+    super(message, cause);
+    this.errorInfo = null;
+  }
 }
