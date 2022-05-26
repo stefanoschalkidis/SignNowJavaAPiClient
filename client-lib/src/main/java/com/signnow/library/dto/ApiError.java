3564061baad5b68baa71e54ea0b838b611177f71
Stefanos Chalkidis <s.chalkidis@protonmail.com>
2022-05-26 16:19:34 +0300

    formatted to google java style

--- client-lib/src/main/java/com/signnow/library/dto/ApiError.java
+++ client-lib/src/main/java/com/signnow/library/dto/ApiError.java
@@ -2,12 +2,12 @@ package com.signnow.library.dto;
 
 import java.util.List;
 
-@SuppressWarnings("java:S1104")  // field name equal to JsonProperty
+@SuppressWarnings("java:S1104") // field name equal to JsonProperty
 public class ApiError {
-    public List<ErrorInfo> errors;
+  public List<ErrorInfo> errors;
 
-    public static class ErrorInfo {
-        public Integer code;
-        public String message;
-    }
+  public static class ErrorInfo {
+    public Integer code;
+    public String message;
+  }
 }
