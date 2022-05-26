0e8c948d1b77a04be3b2b862df37fc595504c8e9
Stefanos Chalkidis <s.chalkidis@protonmail.com>
2022-05-26 16:25:34 +0300

    formatted to google java style

--- client-lib/src/main/java/com/signnow/library/dto/Template.java
+++ client-lib/src/main/java/com/signnow/library/dto/Template.java
@@ -3,28 +3,29 @@ package com.signnow.library.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class Template {
-    public static class CreateRequest {
-        @JsonProperty("document_name")
-        public final String templateName;
-        @JsonProperty("document_id")
-        public final String sourceDocumentId;
+  public static class CreateRequest {
+    @JsonProperty("document_name")
+    public final String templateName;
 
-        public CreateRequest(String templateName, String sourceDocumentId) {
-            this.templateName = templateName;
-            this.sourceDocumentId = sourceDocumentId;
-        }
+    @JsonProperty("document_id")
+    public final String sourceDocumentId;
+
+    public CreateRequest(String templateName, String sourceDocumentId) {
+      this.templateName = templateName;
+      this.sourceDocumentId = sourceDocumentId;
     }
+  }
 
-    public static class CopyRequest {
-        @JsonProperty("document_name")
-        public final String newDocumentName;
+  public static class CopyRequest {
+    @JsonProperty("document_name")
+    public final String newDocumentName;
 
-        public CopyRequest(String newDocumentName) {
-            this.newDocumentName = newDocumentName;
-        }
+    public CopyRequest(String newDocumentName) {
+      this.newDocumentName = newDocumentName;
     }
+  }
 
-    private Template() {
-        throw new IllegalStateException("Utility class");
-    }
+  private Template() {
+    throw new IllegalStateException("Utility class");
+  }
 }
