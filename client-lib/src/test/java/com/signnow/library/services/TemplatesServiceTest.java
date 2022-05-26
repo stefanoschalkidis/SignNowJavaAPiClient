458155b8d1b437ccd45fc483dfbf4e76b72c132b
Stefanos Chalkidis <s.chalkidis@protonmail.com>
2022-05-26 16:43:17 +0300

    formatted to google java style

--- client-lib/src/test/java/com/signnow/library/services/TemplatesServiceTest.java
+++ client-lib/src/test/java/com/signnow/library/services/TemplatesServiceTest.java
@@ -1,44 +1,44 @@
 package com.signnow.library.services;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyMap;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import com.signnow.library.dto.GenericId;
 import com.signnow.library.exceptions.SNException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
-
 class TemplatesServiceTest extends CommonServiceTestCase {
-    private TemplatesService service;
-    @BeforeEach
-    void setUp() {
-        super.setUp();
-        service = new TemplatesService(clientMock);
-    }
+  private TemplatesService service;
 
-    @Test
-    void createTemplate() throws SNException {
-        when(clientMock.post(anyString(), eq(null), any(), eq(GenericId.class))).thenReturn(new GenericId());
+  @BeforeEach
+  void setUp() {
+    super.setUp();
+    service = new TemplatesService(clientMock);
+  }
 
-        service.createTemplate("1", "2");
+  @Test
+  void createTemplate() throws SNException {
+    when(clientMock.post(anyString(), eq(null), any(), eq(GenericId.class)))
+        .thenReturn(new GenericId());
 
-        verify(clientMock, times(1)).post(anyString()
-                , eq(null)
-                , any()
-                , eq(GenericId.class)
-        );
-    }
+    service.createTemplate("1", "2");
 
-    @Test
-    void createDocumentFromTemplate() throws SNException {
-        when(clientMock.post(anyString(), anyMap(), any(), eq(GenericId.class))).thenReturn(new GenericId());
+    verify(clientMock, times(1)).post(anyString(), eq(null), any(), eq(GenericId.class));
+  }
 
-        service.createDocumentFromTemplate("1", "2");
+  @Test
+  void createDocumentFromTemplate() throws SNException {
+    when(clientMock.post(anyString(), anyMap(), any(), eq(GenericId.class)))
+        .thenReturn(new GenericId());
 
-        verify(clientMock, times(1)).post(anyString()
-                , anyMap()
-                , any()
-                , eq(GenericId.class)
-        );
-    }
+    service.createDocumentFromTemplate("1", "2");
+
+    verify(clientMock, times(1)).post(anyString(), anyMap(), any(), eq(GenericId.class));
+  }
 }
