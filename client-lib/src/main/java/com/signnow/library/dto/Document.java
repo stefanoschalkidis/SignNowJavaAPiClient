f547caeca42584201ceb971bae345307227cf596
Stefanos Chalkidis <s.chalkidis@protonmail.com>
2022-05-26 16:21:08 +0300

    formatted to google java style

--- client-lib/src/main/java/com/signnow/library/dto/Document.java
+++ client-lib/src/main/java/com/signnow/library/dto/Document.java
@@ -1,208 +1,225 @@
 package com.signnow.library.dto;
 
-import com.fasterxml.jackson.annotation.*;
-
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonSetter;
 import java.util.ArrayList;
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-@SuppressWarnings("java:S1104")  // field name equal to JsonProperty
+@SuppressWarnings("java:S1104") // field name equal to JsonProperty
 public class Document extends GenericId {
-    @JsonProperty("user_id")
-    public String userId;
-    @JsonProperty("document_name")
-    public String documentName;
-    @JsonProperty("page_count")
-    public String pageCount;
-    public String created;
-    public String updated;
-    @JsonProperty("original_filename")
-    public String originalFilename;
-    @JsonProperty("version_time")
-    public String versionTime;
-    /**
-     * This one stands for some internal data info, does not correspond to document sign ID (free form invite) or group invite ID
-     */
-    @JsonProperty("field_invites")
-    public List<FieldInvite> fieldInvites;
-    /**
-     * Free form invites info
-     */
-    public List<DocumentSignRequestInfo> requests;
+  @JsonProperty("user_id")
+  public String userId;
 
-    public static class SigningLinkRequest {
-        @JsonProperty("document_id")
-        public String documentId;
+  @JsonProperty("document_name")
+  public String documentName;
 
-        public SigningLinkRequest(String documentId) {
-            this.documentId = documentId;
+  @JsonProperty("page_count")
+  public String pageCount;
+
+  public String created;
+  public String updated;
+
+  @JsonProperty("original_filename")
+  public String originalFilename;
+
+  @JsonProperty("version_time")
+  public String versionTime;
+  /**
+   * This one stands for some internal data info, does not correspond to document sign ID (free form
+   * invite) or group invite ID
+   */
+  @JsonProperty("field_invites")
+  public List<FieldInvite> fieldInvites;
+  /** Free form invites info */
+  public List<DocumentSignRequestInfo> requests;
+
+  public static class SigningLinkRequest {
+    @JsonProperty("document_id")
+    public String documentId;
+
+    public SigningLinkRequest(String documentId) {
+      this.documentId = documentId;
+    }
+  }
+
+  @JsonIgnoreProperties(ignoreUnknown = true)
+  @SuppressWarnings("java:S1104") // field name equal to JsonProperty
+  public static class SigningLinkResponse {
+    public String url;
+
+    @JsonProperty("url_no_signup")
+    public String urlNoSignup;
+  }
+
+  @SuppressWarnings("java:S1104") // field name equal to JsonProperty
+  public static class SigningInviteRequest {
+    public final String from;
+    public final String to;
+    public List<String> cc = new ArrayList<>();
+    public String subject;
+    public String message;
+
+    public SigningInviteRequest(String from, String to) {
+      this.from = from;
+      this.to = to;
+    }
+  }
+
+  @SuppressWarnings("java:S1104") // field name equal to JsonProperty
+  public static class SigningInviteWithRolesRequest {
+    public final String from;
+    public final List<InviteRole> to;
+    public List<String> cc = new ArrayList<>();
+    public String subject;
+    public String message;
+
+    public SigningInviteWithRolesRequest(String from, List<InviteRole> to) {
+      this.from = from;
+      this.to = to;
+    }
+  }
+
+  @JsonInclude(JsonInclude.Include.NON_NULL)
+  @SuppressWarnings("java:S1104") // field name equal to JsonProperty
+  public static class InviteRole {
+    public final String email;
+    public final String role;
+
+    @JsonProperty("role_id")
+    public String roleId = "";
+
+    public Integer order = 1;
+    public String password;
+
+    @JsonProperty("expiration_days")
+    public Integer expireAfterDays = null;
+
+    @JsonProperty("reminder")
+    public Integer remindAfterDays = null;
+
+    public InviteRole(String email, String role) {
+      this.email = email;
+      this.role = role;
+    }
+
+    @JsonGetter("authentication_type")
+    public String getAuthType() {
+      if (password != null) {
+        return "password";
+      }
+      return null;
+    }
+  }
+
+  public enum FieldType {
+    SIGNATURE("signature"),
+    TEXT("text"),
+    INITIALS("initials"),
+    CHECKBOX("checkbox"),
+    ENUMERATION("enumeration");
+
+    private final String name;
+
+    FieldType(String name) {
+      this.name = name;
+    }
+
+    @JsonSetter
+    public static FieldType typeOf(String name) {
+      for (FieldType type : values()) {
+        if (type.name.equalsIgnoreCase(name)) {
+          return type;
         }
+      }
+      throw new IllegalArgumentException(name + " field not supported.");
     }
 
-    @JsonIgnoreProperties(ignoreUnknown = true)
-    @SuppressWarnings("java:S1104")  // field name equal to JsonProperty
-    public static class SigningLinkResponse {
-        public String url;
-        @JsonProperty("url_no_signup")
-        public String urlNoSignup;
+    @JsonCreator
+    @Override
+    public String toString() {
+      return name;
     }
+  }
 
-    @SuppressWarnings("java:S1104")  // field name equal to JsonProperty
-    public static class SigningInviteRequest {
-        public final String from;
-        public final String to;
-        public List<String> cc = new ArrayList<>();
-        public String subject;
-        public String message;
+  public static class FieldsUpdateRequest {
+    public final List<Field> fields;
 
-        public SigningInviteRequest(String from, String to) {
-            this.from = from;
-            this.to = to;
-        }
+    public FieldsUpdateRequest(List<Field> fields) {
+      this.fields = fields;
     }
+  }
 
-    @SuppressWarnings("java:S1104")  // field name equal to JsonProperty
-    public static class SigningInviteWithRolesRequest {
-        public final String from;
-        public final List<InviteRole> to;
-        public List<String> cc = new ArrayList<>();
-        public String subject;
-        public String message;
+  @JsonInclude(JsonInclude.Include.NON_NULL)
+  @SuppressWarnings("java:S1104") // field name equal to JsonProperty
+  public static class Field {
+    public int x;
+    public int y;
+    public int width;
+    public int height;
 
-        public SigningInviteWithRolesRequest(String from, List<InviteRole> to) {
-            this.from = from;
-            this.to = to;
-        }
+    @JsonProperty("page_number")
+    public int pageNumber;
+
+    public String role;
+    public boolean required;
+    public FieldType type;
+    public String label;
+
+    @JsonProperty("prefilled_text")
+    public String prefilledText;
+
+    public String getType() {
+      return type.toString();
     }
+  }
 
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    @SuppressWarnings("java:S1104")  // field name equal to JsonProperty
-    public static class InviteRole {
-        public final String email;
-        public final String role;
-        @JsonProperty("role_id")
-        public String roleId = "";
-        public Integer order = 1;
-        public String password;
-        @JsonProperty("expiration_days")
-        public Integer expireAfterDays = null;
-        @JsonProperty("reminder")
-        public Integer remindAfterDays = null;
+  @JsonIgnoreProperties(ignoreUnknown = true)
+  @SuppressWarnings("java:S1104") // field name equal to JsonProperty
+  public static class FieldInvite extends GenericId {
+    public String status;
+    public String email;
+    public String role;
+  }
 
-        public InviteRole(String email, String role) {
-            this.email = email;
-            this.role = role;
-        }
+  @JsonIgnoreProperties(ignoreUnknown = true)
+  public static class DocumentSignRequestInfo extends GenericId {
+    @JsonProperty("signer_email")
+    public String signerEmail;
 
-        @JsonGetter("authentication_type")
-        public String getAuthType() {
-            if (password != null) {
-                return "password";
-            }
-            return null;
-        }
+    @JsonProperty("originator_email")
+    public String originatorEmail;
+  }
+
+  @JsonIgnoreProperties(ignoreUnknown = true)
+  @SuppressWarnings("java:S1104") // field name equal to JsonProperty
+  public static class DocumentDownloadLink {
+    public String link;
+  }
+
+  @JsonIgnoreProperties(ignoreUnknown = true)
+  public static class MoveDocumentRequest {
+    @JsonProperty("folder_id")
+    public String folderId;
+
+    public MoveDocumentRequest(String folderId) {
+      this.folderId = folderId;
     }
+  }
 
-    public enum FieldType {
-        SIGNATURE("signature"),
-        TEXT("text"),
-        INITIALS("initials"),
-        CHECKBOX("checkbox"),
-        ENUMERATION("enumeration");
+  @JsonIgnoreProperties(ignoreUnknown = true)
+  @SuppressWarnings("java:S1104") // field name equal to JsonProperty
+  public static class MoveDocumentResponse {
+    public String result;
+  }
 
-        private final String name;
-
-        FieldType(String name) {
-            this.name = name;
-        }
-
-        @JsonSetter
-        public static FieldType typeOf(String name) {
-            for (FieldType type : values()) {
-                if (type.name.equalsIgnoreCase(name)) {
-                    return type;
-                }
-            }
-            throw new IllegalArgumentException(name + " field not supported.");
-        }
-
-        @JsonCreator
-        @Override
-        public String toString() {
-            return name;
-        }
-    }
-
-    public static class FieldsUpdateRequest {
-        public final List<Field> fields;
-
-        public FieldsUpdateRequest(List<Field> fields) {
-            this.fields = fields;
-        }
-    }
-
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    @SuppressWarnings("java:S1104")  // field name equal to JsonProperty
-    public static class Field {
-        public int x;
-        public int y;
-        public int width;
-        public int height;
-        @JsonProperty("page_number")
-        public int pageNumber;
-        public String role;
-        public boolean required;
-        public FieldType type;
-        public String label;
-        @JsonProperty("prefilled_text")
-        public String prefilledText;
-
-        public String getType() {
-            return type.toString();
-        }
-    }
-
-    @JsonIgnoreProperties(ignoreUnknown = true)
-    @SuppressWarnings("java:S1104")  // field name equal to JsonProperty
-    public static class FieldInvite extends GenericId {
-        public String status;
-        public String email;
-        public String role;
-    }
-
-    @JsonIgnoreProperties(ignoreUnknown = true)
-    public static class DocumentSignRequestInfo extends GenericId {
-        @JsonProperty("signer_email")
-        public String signerEmail;
-        @JsonProperty("originator_email")
-        public String originatorEmail;
-    }
-
-    @JsonIgnoreProperties(ignoreUnknown = true)
-    @SuppressWarnings("java:S1104")  // field name equal to JsonProperty
-    public static class DocumentDownloadLink {
-        public String link;
-    }
-
-    @JsonIgnoreProperties(ignoreUnknown = true)
-    public static class MoveDocumentRequest {
-        @JsonProperty("folder_id")
-        public String folderId;
-
-        public MoveDocumentRequest(String folderId) {
-            this.folderId = folderId;
-        }
-    }
-
-    @JsonIgnoreProperties(ignoreUnknown = true)
-    @SuppressWarnings("java:S1104")  // field name equal to JsonProperty
-    public static class MoveDocumentResponse {
-        public String result;
-    }
-
-    @JsonIgnoreProperties(ignoreUnknown = true)
-    @SuppressWarnings("java:S1104")  // field name equal to JsonProperty
-    public static class DocumentDeleteResponse {
-        public String status;
-    }
+  @JsonIgnoreProperties(ignoreUnknown = true)
+  @SuppressWarnings("java:S1104") // field name equal to JsonProperty
+  public static class DocumentDeleteResponse {
+    public String status;
+  }
 }
