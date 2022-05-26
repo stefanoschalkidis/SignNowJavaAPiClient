b9237162789289c97d81e51ec7f91a2147c50110
Stefanos Chalkidis <s.chalkidis@protonmail.com>
2022-05-26 16:14:36 +0300

    formatted to google java style

--- client-lib/src/main/java/com/signnow/library/Constants.java
+++ client-lib/src/main/java/com/signnow/library/Constants.java
@@ -1,13 +1,13 @@
 package com.signnow.library;
 
 public class Constants {
-    public static final String AUTHORIZATION = "Authorization";
-    public static final String BEARER = "Bearer ";
-    public static final String DOCUMENT_GROUP_ID = "documentGroupId";
-    public static final String DOCUMENT_ID = "documentId";
-    public static final String PATH_TO_DOCUMENT_ID = "/document/{documentId}";  //NOSONAR internal URI
+  public static final String AUTHORIZATION = "Authorization";
+  public static final String BEARER = "Bearer ";
+  public static final String DOCUMENT_GROUP_ID = "documentGroupId";
+  public static final String DOCUMENT_ID = "documentId";
+  public static final String PATH_TO_DOCUMENT_ID = "/document/{documentId}"; // NOSONAR internal URI
 
-    private Constants() {
-        throw new IllegalStateException("Constants class");
-    }
+  private Constants() {
+    throw new IllegalStateException("Constants class");
+  }
 }
