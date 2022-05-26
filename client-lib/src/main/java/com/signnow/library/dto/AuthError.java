e9c2cfc369b5d2a21fe84768df53159d536c9aa9
Stefanos Chalkidis <s.chalkidis@protonmail.com>
2022-05-26 16:20:06 +0300

    formatted to google java style

--- client-lib/src/main/java/com/signnow/library/dto/AuthError.java
+++ client-lib/src/main/java/com/signnow/library/dto/AuthError.java
@@ -2,42 +2,42 @@ package com.signnow.library.dto;
 
 import com.fasterxml.jackson.annotation.JsonSetter;
 
-@SuppressWarnings("java:S1104")  // field name equal to JsonProperty
+@SuppressWarnings("java:S1104") // field name equal to JsonProperty
 public class AuthError {
-    public Type error;
-    public Integer code;
+  public Type error;
+  public Integer code;
 
-    @JsonSetter("error")
-    public void setType(String type) {
-        this.error = Type.typeOf(type);
+  @JsonSetter("error")
+  public void setType(String type) {
+    this.error = Type.typeOf(type);
+  }
+
+  public enum Type {
+    INVALID_CLIENT("invalid_client"),
+    INVALID_REQUEST("invalid_request"),
+    INVALID_TOKEN("invalid_token"),
+    INVALID_CREDENTIALS("Invalid credentials."),
+    ACCESS_DENIED("access_denied"),
+    UNKNOWN("unknown");
+
+    private final String name;
+
+    Type(String name) {
+      this.name = name;
     }
 
-    public enum Type {
-        INVALID_CLIENT("invalid_client"),
-        INVALID_REQUEST("invalid_request"),
-        INVALID_TOKEN("invalid_token"),
-        INVALID_CREDENTIALS("Invalid credentials."),
-        ACCESS_DENIED("access_denied"),
-        UNKNOWN("unknown");
-
-        private final String name;
-
-        Type(String name) {
-            this.name = name;
+    public static Type typeOf(String name) {
+      for (Type type : values()) {
+        if (type.name.equalsIgnoreCase(name)) {
+          return type;
         }
-
-        public static Type typeOf(String name) {
-            for (Type type : values()) {
-                if (type.name.equalsIgnoreCase(name)) {
-                    return type;
-                }
-            }
-            return UNKNOWN;
-        }
-
-        @Override
-        public String toString() {
-            return name;
-        }
+      }
+      return UNKNOWN;
     }
+
+    @Override
+    public String toString() {
+      return name;
+    }
+  }
 }
